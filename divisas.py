842eea5a2f62d2cff50dd76ec023f993319061b8
gustavo1245 <162451133+gustavo1245@users.noreply.github.com>
2025-03-25 12:37:39 -0300

    Add files via upload

---
+++ divisas.py
@@ -0,0 +1,11 @@
+#gustavo soto
+#takitaki
+#25-03-2025
+
+pesos = int(input("Cuanto te queda en pesos colombianos?"))
+soles = int(input("Cuanto te queda en soles peruanos?"))
+reales = int(input("Cuanto te queda en reales brazileños?"))
+
+total = pesos * 0.00025 + soles * 0.28 + reales * 0.21
+
+print (total)
---
+++ temperatura.py
@@ -0,0 +1,9 @@
+#gustavo soto
+#minimarcket
+#25-03-2025
+
+temp_f = 59
+
+temp_c = (temp_f - 32) / 1.8
+
+print(temp_c)
